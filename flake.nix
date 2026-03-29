34a485a7862804ba0c8660c42db97e35e81e0039
hand7s <117505144+s0me1newithhand7s@users.noreply.github.com>
2026-03-30 01:53:01 +0300

    flake.nix: shorting

--- flake.nix
+++ flake.nix
@@ -1,26 +1,14 @@
 {
   description = "miniflake.";
-  inputs = {
-    nixpkgs = {
-      url = "github:nixos/nixpkgs/nixos-unstable";
-    };
-  };
+  inputs."nixpkgs".url = "github:nixos/nixpkgs/nixos-unstable";
   
-  outputs = {
-    self,
-    nixpkgs,
-    ...
-  } @ inputs: {
+  outputs = inputs @{nixpkgs}: {
     nixosConfigurations ."default" = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-        modules = [
-          (
-            {
-              fileSystems."/".device = "/dev/null";
-              boot.loader.systemd-boot.enable = true;
-            }
-          )
-        ];
+        modules = [({
+          fileSystems."/".device = "/dev/null";
+          boot.loader.systemd-boot.enable = true;
+        })];
       };
     };
 }
